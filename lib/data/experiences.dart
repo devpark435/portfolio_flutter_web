3ed4fe21413855f4e63055d626c6781978920b96
devpark435 <devpark435@gmail.com>
2025-02-25 18:07:41 +0900

    refactor - 시간 역순으로 변경

--- lib/data/experiences.dart
+++ lib/data/experiences.dart
@@ -2,34 +2,21 @@ import '../domain/models/experience.dart';
 
 final List<Experience> experiences = [
   const Experience(
-    title: 'GDSC 성공회대 1기',
-    role: 'Flutter 개발',
-    period: '2022.03 - 2023.03',
-    description: [
-      '팀 프로젝트 3개 리드',
-      '연합 해커톤 3회 참여',
-      '동아리 대상 Flutter 반응형 UI 교육 진행',
-      'Google Solution Challenge 참여',
-      '2기 대상 모바일 세션 진행'
-    ],
-  ),
-  const Experience(
-    title: '교내 동아리 코딩보틀',
-    role: '모바일 개발',
-    period: '2023.08 - 2024.03',
-    description: [
-      '동아리 대상 iOS 교육 진행',
-      '코딩 테스트, 면접 소모임 진행',
-      '팀 프로젝트 리드 Copro 앱 스토어 배포'
-    ],
-  ),
-  const Experience(
-    title: '프리랜서 외주 프로젝트',
+    title: '프랜차이즈 웹 외주 프로젝트',
     role: '프론트엔드 개발자',
-    period: '2023.08 - 2024.04',
+    period: '2025.01 - 진행 중',
     description: [
-      '심리 테스트 웹사이트 제작',
+      '프렌차이즈 홍보 목적 브랜딩 페이지 제작',
       'Flutter Web 활용한 반응형 디자인 구현',
+    ],
+  ),
+  const Experience(
+    title: '매장 관리 시스템 외주 프로젝트',
+    role: '프론트엔드 개발자',
+    period: '2024.09 - 2024.11',
+    description: [
+      '매장 재고 관리, 주문 관리 시스템 개발',
+      'Flutter 활용한 iOS, Android 어플리케이션 제작',
     ],
   ),
   const Experience(
@@ -44,21 +31,34 @@ final List<Experience> experiences = [
     ],
   ),
   const Experience(
-    title: '프리랜서 외주 프로젝트',
+    title: '심리 테스트 웹 외주 프로젝트',
     role: '프론트엔드 개발자',
-    period: '2024.09 - 2024.11',
+    period: '2023.08 - 2024.04',
     description: [
-      '주문 관리 시스템 개발',
-      'Flutter 활용한 iOS, Android 어플리케이션 제작',
+      '심리 테스트 웹사이트 제작',
+      'Flutter Web 활용한 반응형 디자인 구현',
     ],
   ),
   const Experience(
-    title: '프리랜서 외주 프로젝트',
-    role: '프론트엔드 개발자',
-    period: '2025.01 - 진행 중',
+    title: '교내 동아리 코딩보틀',
+    role: '모바일 개발',
+    period: '2023.08 - 2024.03',
     description: [
-      '프렌차이즈 홍보 목적 브랜딩 페이지 제작',
-      'Flutter Web 활용한 반응형 디자인 구현',
+      '동아리 대상 iOS 교육 진행',
+      '코딩 테스트, 면접 소모임 진행',
+      '팀 프로젝트 리드 Copro 앱 스토어 배포'
+    ],
+  ),
+  const Experience(
+    title: 'GDSC 성공회대 1기',
+    role: 'Flutter 개발',
+    period: '2022.03 - 2023.03',
+    description: [
+      '팀 프로젝트 3개 리드',
+      '연합 해커톤 3회 참여',
+      '동아리 대상 Flutter 반응형 UI 교육 진행',
+      'Google Solution Challenge 참여',
+      '2기 대상 모바일 세션 진행'
     ],
   ),
 ];
