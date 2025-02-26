fd690c1475641de11df775f264970c2fd48b6f19
devpark435 <devpark435@gmail.com>
2025-02-26 18:41:12 +0900

    fix - 불필요한 import 제거

--- lib/presentation/widgets/project_card.dart
+++ lib/presentation/widgets/project_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import '../../domain/models/project.dart';
 import '../screens/project_detail/project_detail_screen.dart';
 
